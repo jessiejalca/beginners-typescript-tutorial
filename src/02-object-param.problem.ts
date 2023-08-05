780172314c325a0cc6a6a70f6b851bcc57fa6644
Jessie Jalca <jessiejalca@gmail.com>
2023-08-04 22:40:38 -0400

    fix: Solve ex02

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,21 +1,33 @@
-import { expect, it } from "vitest";
+import { expect, it } from "vitest"
 
-export const addTwoNumbers = (params) => {
-  return params.first + params.second;
-};
+// Option 1
+export const addTwoNumbers = (params: { first: number; second: number }) => {
+  return params.first + params.second
+}
+
+// Option 2: Use "type" or "interface" alias
+type TwoNumbers = {
+  first: number
+  second: number
+}
+/*
+export const addTwoNumbers = (params: TwoNumbers) => {
+  return params.first + params.second
+}
+*/
 
 it("Should add the two numbers together", () => {
   expect(
     addTwoNumbers({
       first: 2,
       second: 4,
-    }),
-  ).toEqual(6);
+    })
+  ).toEqual(6)
 
   expect(
     addTwoNumbers({
       first: 10,
       second: 20,
-    }),
-  ).toEqual(30);
-});
+    })
+  ).toEqual(30)
+})
