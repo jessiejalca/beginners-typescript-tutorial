c9369734b79916dbc56bafce7e08e1fd0ecac563
Jessie Jalca <jessiejalca@gmail.com>
2023-08-04 22:43:31 -0400

    fix: Solve ex03

--- src/03-optional-properties.problem.ts
+++ src/03-optional-properties.problem.ts
@@ -1,25 +1,26 @@
-import { expect, it } from "vitest";
+import { expect, it } from "vitest"
 
-export const getName = (params: { first: string; last: string }) => {
+// Mark properties as optional with a ? at the end of the name
+export const getName = (params: { first: string; last?: string }) => {
   if (params.last) {
-    return `${params.first} ${params.last}`;
+    return `${params.first} ${params.last}`
   }
-  return params.first;
-};
+  return params.first
+}
 
 it("Should work with just the first name", () => {
   const name = getName({
     first: "Matt",
-  });
+  })
 
-  expect(name).toEqual("Matt");
-});
+  expect(name).toEqual("Matt")
+})
 
 it("Should work with the first and last name", () => {
   const name = getName({
     first: "Matt",
     last: "Pocock",
-  });
+  })
 
-  expect(name).toEqual("Matt Pocock");
-});
+  expect(name).toEqual("Matt Pocock")
+})
