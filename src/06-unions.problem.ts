617018a2baa445c86df08e0ecbc3979780821015
Jessie Jalca <jessiejalca@gmail.com>
2023-08-09 23:07:19 -0400

    fix: Solve ex06

--- src/06-unions.problem.ts
+++ src/06-unions.problem.ts
@@ -8,13 +8,19 @@ interface User {
    * - 'user'
    * - 'super-admin'
    */
-  role: string;
+  // these are called union types
+  // you can put all sorts of stuff in here,
+  // including objects, other interfaces/types, etc.,
+  // and they're super helpful in providing extra
+  // safety in your code
+  role: "admin" | "user" | "super-admin";
 }
 
 export const defaultUser: User = {
   id: 1,
   firstName: "Matt",
   lastName: "Pocock",
+  // use the next line to check if errors are being caught properly
   // @ts-expect-error
   role: "I_SHOULD_NOT_BE_ALLOWED",
 };
