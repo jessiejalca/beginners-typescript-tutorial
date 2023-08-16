b00a8f3b961ba5307f83a6fef4ffb3992639d832
Jessie Jalca <jessiejalca@gmail.com>
2023-08-15 21:37:29 -0400

    fix: Solve ex09

--- src/09-promises.problem.ts
+++ src/09-promises.problem.ts
@@ -9,10 +9,23 @@ interface LukeSkywalker {
   gender: string;
 }
 
-export const fetchLukeSkywalker = async (): LukeSkywalker => {
+export const fetchLukeSkywalker = async () => {
+  // These two options are probably better than
+  // ... = async (): Promise<LukeSkywalker> ...
+  // because it will infer that anyway when you
+  // use these other methods:
+
+  // METHOD 2:
+  // This also works; it tells TS the shape of the
+  // data that should be coming in:
+  // const data: LukeSkywalker = await fetch...
   const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
     return res.json();
   });
 
-  return data;
+  // METHOD 1:
+  // This is a good way to let TypeScript know what
+  // data to expect while still knowing it doesn't
+  // technically know for sure what data will come in:
+  return data as LukeSkywalker;
 };
