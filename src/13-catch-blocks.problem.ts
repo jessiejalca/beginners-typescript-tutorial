fe62c19c182336e40c685978326164e8bc568013
Jessie Jalca <jessiejalca@gmail.com>
2023-08-16 22:58:24 -0400

    fix: Solve ex13

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -1,15 +1,21 @@
-import { expect, it } from "vitest";
+import { expect, it } from "vitest"
 
 const tryCatchDemo = (state: "fail" | "succeed") => {
   try {
     if (state === "fail") {
-      throw new Error("Failure!");
+      throw new Error("Failure!")
     }
   } catch (e) {
-    return e.message;
+    // this is the safest solution, checking if
+    // e is actually an error, so that we get
+    // autocomplete without typecasting it as
+    // an error when it's possible it could not be
+    if (e instanceof Error) {
+      return e.message
+    }
   }
-};
+}
 
 it("Should return the message when it fails", () => {
-  expect(tryCatchDemo("fail")).toEqual("Failure!");
-});
+  expect(tryCatchDemo("fail")).toEqual("Failure!")
+})
