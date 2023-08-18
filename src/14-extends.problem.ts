31859ffa5ae8729e3394d5568cb37aa8b8b1beba
Jessie Jalca <jessiejalca@gmail.com>
2023-08-18 17:56:58 -0400

    fix: Solve ex14

--- src/14-extends.problem.ts
+++ src/14-extends.problem.ts
@@ -1,4 +1,4 @@
-import { Equal, Expect } from "./helpers/type-utils";
+import { Equal, Expect } from "./helpers/type-utils"
 
 /**
  * Here, the id property is shared between all three
@@ -6,25 +6,31 @@ import { Equal, Expect } from "./helpers/type-utils";
  * make it more DRY?
  */
 
-interface User {
-  id: string;
-  firstName: string;
-  lastName: string;
+interface Base {
+  id: string
 }
 
-interface Post {
-  id: string;
-  title: string;
-  body: string;
+// - interfaces can extend from other interfaces,
+// like a class; this isn't something type can do
+// - this is great for expressing relationships
+// between interfaces
+
+interface User extends Base {
+  firstName: string
+  lastName: string
 }
 
-interface Comment {
-  id: string;
-  comment: string;
+interface Post extends Base {
+  title: string
+  body: string
+}
+
+interface Comment extends Base {
+  comment: string
 }
 
 type tests = [
   Expect<Equal<User, { id: string; firstName: string; lastName: string }>>,
   Expect<Equal<Post, { id: string; title: string; body: string }>>,
-  Expect<Equal<Comment, { id: string; comment: string }>>,
-];
+  Expect<Equal<Comment, { id: string; comment: string }>>
+]
